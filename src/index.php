0dc53e0d1ed377dbc61e463810022c3eed07e5f5
Justine Paralejas <paralejasjustine15@gmail.com>
2024-08-14 10:57:27 -0700

    temporary id template

--- src/index.php
+++ src/index.php
@@ -1,11 +1,21 @@
 <?php
   require('./temporaryId.php');
-  $name = 'Lorem Ipsum';
+  $name = 'Lorem Ipsum de amet lor de sur';
   $id = '00-0000';
-  $course = 'Bachelor of Science in Computer Engineering Testasdkj tqwelkj asd';
+  $course = 'Bachelor of Science in Computer Engineering';
   $image = '../assets/images/panda.png';
+  $instructions = [
+    '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    '4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    '5. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  ];
+
+  $fillColor = [255, 127, 80];
+
   generateTemporaryId(
-    $name, $id, $image, $course
+    $name, $id, $image, $course, $instructions, $fillColor
   );
 
 ?>
--- src/temporaryId.php
+++ src/temporaryId.php
@@ -1,32 +1,63 @@
 <?php
 require('tempIdTemplate.php');
-function generateTemporaryId($name, $idNumber, $image, $course) {
-    $id = new ID('P', 'mm', array(85.60, 53.98));
+function generateTemporaryId($name, $idNumber, $image, $course, $instructions, $fillColor) {
+    $id = new ID('P', 'mm', 'Letter');
 
     $id->AddPage();
-    $id->skipFooter = false;
+    $id->skipFooter = true;
 
-    $imageWidth = 18;
-    $imageHeight = 18;
+    $imageWidth = 30;
+    $imageHeight = 40;
     $pageWidth = $id->GetPageWidth();
     $pageHeight = $id->GetPageHeight();
 
-    $centerX = ($pageWidth - $imageWidth) / 2;
+    // $centerX = ($pageWidth - $imageWidth) / 2;
     $centerY = ($pageHeight - $imageHeight) / 2;
     
-    $id->Image($image, $centerX, 20, $imageWidth, $imageHeight);
+    $id->SetFont('Arial', 'B', 13);
+    $id->setY(20);
+    $id->MultiCell($pageWidth, 10, 'Instructions', 0, 'L');
 
-    // $id->Rect(13, 20, 25, 25);
+    $id->SetFont('Arial', '', 13);
+    foreach ($instructions as $instruction) {
+        $id->MultiCell($pageWidth - 30, 10, $instruction, 0, 'L');
+    }
 
-    $id->SetFont('Arial', 'B', 10);
-    $id->SetY(40);
-    $id->Cell(0, 5, $name, 0, 1, 'C');
+    $textStart = $centerY + 22;
+    $id->SetY($textStart);
+    $id->Image('../assets/images/cdm-logo.png', 42, $textStart, 10, 10);
 
-    $id->SetFont('Arial', '', 5);
-    $id->Cell(0, 3, $idNumber, 0, 1, 'C');
+    $id->SetFont('Arial', '', 10);
+    $id->MultiCell(74, 9, 'Colegio de Montalban', 0, 'R');
 
-    $id->SetX(0);
-    $id->MultiCell($pageWidth, 3, $course, 0, 'C');
+    $id->SetFont('Arial', '', 3);
+    $id->MultiCell(65, -3, 'Kasiglahan Village, San Jose, Montalban, Rizal 1860', 0, 'R');
+
+    $id->RoundedRect(40, $centerY + 20, 53.98, 85.6, 3, '1234', 'D');
+    $id->RoundedRect(120, $centerY + 20, 53.98, 85.6, 3, '1234', 'D');
+
+    $id->Image($image, 51, $textStart + 10, $imageWidth, $imageHeight);
+
+    $id->SetXY(43, $textStart + 53);
+
+    $id->SetFont('Arial', 'B', 12);
+    $id->MultiCell(48, 4, $name, 0, 'C');
+
+    $id->SetXY(45, $textStart + 63);
+
+    $id->SetFont('Arial', '', 6);
+    $id->MultiCell(40, 4, $idNumber, 0, 'C');
+
+    $id->SetXY(40, $textStart + 67);
+
+    $id->SetFont('Arial', '', 7);
+    $id->SetFillColor($fillColor[0], $fillColor[1], $fillColor[2]);
+    $id->MultiCell(54, 4, $course, 0, 1, 'C');
+
+    $id->SetXY(37, $textStart + 75);
+
+    $id->SetFont('Arial', 'B', 7);
+    $id->MultiCell(60, 4, 'TEMPORARY ID', 0, 'C');
     
     $outputPath = '../temporaryIDs/';
     if (!file_exists($outputPath)) mkdir($outputPath, 0777, true);
